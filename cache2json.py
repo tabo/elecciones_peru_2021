34a80be25061421e9b31c00397ff2bda09cb003d
Gustavo Picon <tabo@tabo.pe>
2021-06-17 21:10:26 -0700

    Revirtiendo cambios a scripts ronderos

--- cache2json.py
+++ cache2json.py
@@ -22,31 +22,21 @@ class Cache2Json:
                 "mesas": {},
                 "locales": defaultdict(dict)
             }
-            # logging.info("Procesando actas")
-            # for json_file in (election_dir / "mesas/actas/11/").glob("**/*.json"):
-            #     data["locales"][json_file.parent.stem][json_file.stem] = json.loads(json_file.read_text())
-            # logging.info("Procesando ubigeos")
-            # for json_file in (election_dir / "ecp/ubigeos").glob("*.json"):
-            #     data["geo_regions"][json_file.stem] = json.loads(json_file.read_text())
-            # logging.info("Procesando locales")
-            # for json_file in (election_dir / "mesas/locales").glob("*.json"):
-            #     data["ubigeos"][json_file.stem] = json.loads(json_file.read_text())
-            # logging.info("Procesando detalles")
+            logging.info("Procesando actas")
+            for json_file in (election_dir / "mesas/actas/11/").glob("**/*.json"):
+                data["locales"][json_file.parent.stem][json_file.stem] = json.loads(json_file.read_text())
+            logging.info("Procesando ubigeos")
+            for json_file in (election_dir / "ecp/ubigeos").glob("*.json"):
+                data["geo_regions"][json_file.stem] = json.loads(json_file.read_text())
+            logging.info("Procesando locales")
+            for json_file in (election_dir / "mesas/locales").glob("*.json"):
+                data["ubigeos"][json_file.stem] = json.loads(json_file.read_text())
+            logging.info("Procesando detalles")
             for json_file in (election_dir / "mesas/detalle").glob("*.json"):
-                text = json_file.read_text()
-                detalle_data = json.loads(text)
-                if text.count("\n") <= 10:
-                    logging.info(json_file)
-                    for proceso_data in detalle_data.get("procesos", {}).values():
-                        if isinstance(proceso_data, dict):
-                            proceso_data.pop("imageActa", None)
-                            for resol in proceso_data.get("resoluciones", []):
-                                resol.pop("IMAGEN", None)
-                    json_file.write_text(json.dumps(detalle_data, sort_keys=True, indent=4))
-                data["mesas"][json_file.stem] = detalle_data
+                data["mesas"][json_file.stem] = json.loads(json_file.read_text())
         data_file = self.base_dir / "data.json"
         logging.info(f"Ronderos guardando data en {data_file}")
-        # data_file.write_text(json.dumps(self.data, sort_keys=True, indent=4))
+        data_file.write_text(json.dumps(self.data, sort_keys=True, indent=4))
 
 
 def main():
