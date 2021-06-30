1c3b0709b293474343b0b59f1ab9fe55d8cf5c10
Gustavo Picon <tabo@tabo.pe>
2021-06-29 21:14:17 -0700

    Actualizando 🤡criptomodelo🤡 version 2
    fuente: https://drive.google.com/drive/folders/1juVgcNIZbJQU17GYYuaXS_Cw0GhwM1XQ

--- sql/insert_into_payasada_reloaded.sql
+++ sql/insert_into_payasada_reloaded.sql
@@ -1,17 +1,12 @@
-INSERT INTO payasada
+INSERT INTO payasada_reloaded
 SELECT *,
-       (NOT anulada_payaso AND acta_falsa_perulibre = 'Hallazgo')         AS hallazgo_perulibre_payaso,
-       (NOT anulada_corregido AND acta_falsa_perulibre = 'Hallazgo')      AS hallazgo_perulibre_corregido,
-       (NOT anulada_payaso AND acta_falsa_perulibre = 'Sospechoso')       AS sospechoso_perulibre_payaso,
-       (NOT anulada_corregido AND acta_falsa_perulibre = 'Sospechoso')    AS sospechoso_perulibre_corregido,
-       (NOT anulada_payaso AND acta_falsa_fp_payaso = 'Hallazgo')         AS hallazgo_fp_payaso,
-       (NOT anulada_corregido AND acta_falsa_fp_corregido = 'Hallazgo')   AS hallazgo_fp_corregido,
-       (NOT anulada_payaso AND acta_falsa_fp_payaso = 'Sospechoso')       AS sospechoso_fp_payaso,
-       (NOT anulada_corregido AND acta_falsa_fp_corregido = 'Sospechoso') AS sospechoso_fp_corregido
+       (NOT anulada AND acta_falsa_perulibre = 'Hallazgo')   AS hallazgo_perulibre,
+       (NOT anulada AND acta_falsa_perulibre = 'Sospechoso') AS sospechoso_perulibre,
+       (NOT anulada AND acta_falsa_fp = 'Hallazgo')          AS hallazgo_fp,
+       (NOT anulada AND acta_falsa_fp = 'Sospechoso')        AS sospechoso_fp
 FROM (
          SELECT *,
-                (acta_falsa_perulibre = 'Hallazgo' AND acta_falsa_fp_payaso = 'Hallazgo')    AS anulada_payaso,
-                (acta_falsa_perulibre = 'Hallazgo' AND acta_falsa_fp_corregido = 'Hallazgo') AS anulada_corregido
+                (acta_falsa_perulibre = 'Hallazgo' AND acta_falsa_fp = 'Hallazgo') AS anulada
 
          FROM (
                   SELECT *,
@@ -25,20 +20,12 @@ FROM (
                              ) AS esc3_desc_perulibre,
                          (
                              CASE
-                                 WHEN esc3_prob_fp_payaso < 0.001 THEN 'Imposible'
-                                 WHEN esc3_prob_fp_payaso < 0.01 THEN 'Improbable'
-                                 WHEN esc3_prob_fp_payaso < 0.1 THEN 'Muy Poco Probable'
+                                 WHEN esc3_prob_fp < 0.001 THEN 'Imposible'
+                                 WHEN esc3_prob_fp < 0.01 THEN 'Improbable'
+                                 WHEN esc3_prob_fp < 0.1 THEN 'Muy Poco Probable'
                                  ELSE ''
                                  END
-                             ) AS esc3_desc_fp_payaso,
-                         (
-                             CASE
-                                 WHEN esc3_prob_fp_corregido < 0.001 THEN 'Imposible'
-                                 WHEN esc3_prob_fp_corregido < 0.01 THEN 'Improbable'
-                                 WHEN esc3_prob_fp_corregido < 0.1 THEN 'Muy Poco Probable'
-                                 ELSE ''
-                                 END
-                             ) AS esc3_desc_fp_corregido,
+                             ) AS esc3_desc_fp,
                          (
                              CASE
                                  WHEN v2_blanco = 0 THEN 'Alerta por Blancos'
@@ -62,63 +49,40 @@ FROM (
                              ) AS acta_falsa_perulibre,
                          (
                              CASE
-                                 WHEN esc3_prob_fp_payaso < 0.1 THEN (
+                                 WHEN esc3_prob_fp < 0.1 THEN (
                                      CASE
-                                         WHEN NOT cumplimiento_esc2_fp_payaso
+                                         WHEN NOT cumplimiento_esc2_fp
                                              THEN 'Hallazgo'
-                                         WHEN NOT cumplimiento_esc1_fp_payaso
+                                         WHEN NOT cumplimiento_esc1_fp
                                              THEN 'Sospechoso'
                                          ELSE ''
                                          END
                                      )
                                  ELSE ''
                                  END
-                             ) AS acta_falsa_fp_payaso,
-                         (
-                             CASE
-                                 WHEN esc3_prob_fp_corregido < 0.1 THEN (
-                                     CASE
-                                         WHEN NOT cumplimiento_esc2_fp_corregido
-                                             THEN 'Hallazgo'
-                                         WHEN NOT cumplimiento_esc1_fp_corregido
-                                             THEN 'Sospechoso'
-                                         ELSE ''
-                                         END
-                                     )
-                                 ELSE ''
-                                 END
-                             ) AS acta_falsa_fp_corregido
+                             ) AS acta_falsa_fp
                   FROM (
                            SELECT *,
-                                  v2_perulibre * 1.1 > mm_perulibre * 0.9                                    AS cumplimiento_mm_perulibre,
-                                  v2_fp * 1.1 > mm_fp_payaso * 0.9                                           AS cumplimiento_mm_fp_payaso,
-                                  v2_fp * 1.1 > mm_fp_corregido * 0.9                                        AS cumplimiento_mm_fp_corregido,
-                                  FLOOR(mm_perulibre * 0.5)                                                  AS esc1_perulibre,
-                                  FLOOR(mm_fp_payaso * 0.5)                                                  AS esc1_fp_payaso,
-                                  FLOOR(mm_fp_corregido * 0.5)                                               AS esc1_fp_corregido,
-                                  v2_perulibre > FLOOR(mm_perulibre * 0.5)                                   AS cumplimiento_esc1_perulibre,
-                                  v2_fp > FLOOR(mm_fp_payaso * 0.5)                                          AS cumplimiento_esc1_fp_payaso,
-                                  v2_fp > FLOOR(mm_fp_corregido * 0.5)                                       AS cumplimiento_esc1_fp_corregido,
-                                  FLOOR(mm_perulibre * 0.2)                                                  AS esc2_perulibre,
-                                  FLOOR(mm_fp_payaso * 0.2)                                                  AS esc2_fp_payaso,
-                                  FLOOR(mm_fp_corregido * 0.2)                                               AS esc2_fp_corregido,
-                                  v2_perulibre > FLOOR(mm_perulibre * 0.2)                                   AS cumplimiento_esc2_perulibre,
-                                  v2_fp > FLOOR(mm_fp_payaso * 0.2)                                          AS cumplimiento_esc2_fp_payaso,
-                                  v2_fp > FLOOR(mm_fp_corregido * 0.2)                                       AS cumplimiento_esc2_fp_corregido,
+                                  v2_perulibre * 1.1 > mm_perulibre * 0.9                AS cumplimiento_mm_perulibre,
+                                  v2_fp * 1.1 > mm_fp * 0.9                              AS cumplimiento_mm_fp,
+                                  FLOOR(mm_perulibre * 0.5)                              AS esc1_perulibre,
+                                  FLOOR(mm_fp * 0.5)                                     AS esc1_fp,
+                                  v2_perulibre > FLOOR(mm_perulibre * 0.5)               AS cumplimiento_esc1_perulibre,
+                                  v2_fp > FLOOR(mm_fp * 0.5)                             AS cumplimiento_esc1_fp,
+                                  FLOOR(mm_perulibre * 0.2)                              AS esc2_perulibre,
+                                  FLOOR(mm_fp * 0.2)                                     AS esc2_fp,
+                                  v2_perulibre > FLOOR(mm_perulibre * 0.2)               AS cumplimiento_esc2_perulibre,
+                                  v2_fp > FLOOR(mm_fp * 0.2)                             AS cumplimiento_esc2_fp,
                                   CASE
                                       WHEN v2_perulibre >= mm_perulibre THEN 0
-                                      ELSE mm_perulibre - v2_perulibre END                                   AS delta_perulibre,
-                                  CASE WHEN v2_fp >= mm_fp_payaso THEN 0 ELSE mm_fp_payaso - v2_fp END       AS delta_fp_payaso,
-                                  CASE WHEN v2_fp >= mm_fp_corregido THEN 0 ELSE mm_fp_corregido - v2_fp END AS delta_fp_corregido,
+                                      ELSE mm_perulibre - v2_perulibre END               AS delta_perulibre,
+                                  CASE WHEN v2_fp >= mm_fp THEN 0 ELSE mm_fp - v2_fp END AS delta_fp,
                                   CASE
                                       WHEN v2_perulibre >= mm_perulibre THEN 1
-                                      ELSE POWER(0.8, mm_perulibre - v2_perulibre) END                       AS esc3_prob_perulibre,
+                                      ELSE POWER(0.8, mm_perulibre - v2_perulibre) END   AS esc3_prob_perulibre,
                                   CASE
-                                      WHEN v2_fp >= mm_fp_payaso THEN 1
-                                      ELSE POWER(0.8, mm_fp_payaso - v2_fp) END                              AS esc3_prob_fp_payaso,
-                                  CASE
-                                      WHEN v2_fp >= mm_fp_corregido THEN 1
-                                      ELSE POWER(0.8, mm_fp_corregido - v2_fp) END                           AS esc3_prob_fp_corregido
+                                      WHEN v2_fp >= mm_fp THEN 1
+                                      ELSE POWER(0.8, mm_fp - v2_fp) END                 AS esc3_prob_fp
                            FROM (
                                     SELECT p.mesa,
                                            p.v2_CCODI_UBIGEO,
@@ -153,27 +117,6 @@ FROM (
                                                            0
                                                ) AS mm_perulibre,
                                            FLOOR(
-                                                           p.v1_fa * r.r_pnp_fp +
-                                                           p.v1_morado * r.r_fa_fp +
-                                                           p.v1_pps * r.r_morado_fp +
-                                                           p.v1_vn * r.r_pps_fp +
-                                                           p.v1_ap * r.r_vn_fp +
-                                                           p.v1_avpais * r.r_ap_fp +
-                                                           p.v1_pp * r.r_avpais_fp +
-                                                           p.v1_jpp * r.r_pp_fp +
-                                                           p.v1_ppc * r_jpp_fp +
-                                                           p.v1_fp * r_ppc_fp +
-                                                           p.v1_upp * r_fp_fp +
-                                                           p.v1_rp * r_upp_fp +
-                                                           p.v1_runa * r_rp_fp +
-                                                           p.v1_sp * r_runa_fp +
-                                                           p.v1_perulibre * r_sp_fp +
-                                                           p.v1_dd * r_perulibre_fp +
-                                                           p.v1_app * r_dd_fp +
-                                                           p.v1_blanco * r_app_fp +
-                                                           0
-                                               ) AS mm_fp_payaso,
-                                           FLOOR(
                                                            p.v1_pnp * r.r_pnp_fp +
                                                            p.v1_fa * r.r_fa_fp +
                                                            p.v1_morado * r.r_morado_fp +
@@ -193,9 +136,9 @@ FROM (
                                                            p.v1_dd * r_dd_fp +
                                                            p.v1_app * r_app_fp +
                                                            0
-                                               ) AS mm_fp_corregido
+                                               ) AS mm_fp
                                     FROM presidencial AS p
-                                             INNER JOIN ratios_de_payaso AS r ON p.v2_DEPARTAMENTO = r.departamento
+                                             INNER JOIN ratios_de_payaso2 AS r ON p.v2_DEPARTAMENTO = r.departamento
                                 )
                        )
               )
